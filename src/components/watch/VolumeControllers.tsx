77746395faded141094977221f80f7f863f327b9
Kaloyan Georgiev <Swellboyvn@gmail.com>
2024-04-05 21:48:41 +0300

    Fix types errors

--- src/components/watch/VolumeControllers.tsx
+++ src/components/watch/VolumeControllers.tsx
@@ -1,7 +1,7 @@
+import React from 'react';
 import { Stack } from "@mui/material";
 import Slider from "@mui/material/Slider";
 import { styled } from "@mui/material/styles";
-import { SliderUnstyledOwnProps } from "@mui/base/SliderUnstyled";
 import VolumeUpIcon from "@mui/icons-material/VolumeUp";
 import VolumeOffIcon from "@mui/icons-material/VolumeOff";
 import PlayerControlButton from "./PlayerControlButton";
@@ -41,7 +41,7 @@ export default function VolumeControllers({
   muted,
 }: {
   value: number;
-  handleVolume: SliderUnstyledOwnProps["onChange"];
+  handleVolume: (event: Event, newValue: number | number[]) => void;
   handleVolumeToggle: React.MouseEventHandler<HTMLButtonElement>;
   muted: boolean;
 }) {
